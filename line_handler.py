35d2aecce9c535b98b95f9a8832da296da9d55ab
1101416 <103500373+1101416@users.noreply.github.com>
2025-10-24 16:50:20 +0800

    Update line_handler.py

--- line_handler.py
+++ line_handler.py
@@ -22,6 +22,7 @@ TEXTS = {
     'zh-TW': {
         'welcome_title': '🌱 AI 智能垃圾分類助手',
         'welcome_body': '歡迎使用！\n我可以幫您：\n• 📸 拍照識別垃圾類型\n• 📍 傳送位置查詢附近垃圾車\n\n請拍照上傳或傳送您的位置！',
+        'lang_selected': '🌎Language has been set to: English',
         'help': """📖 使用說明
 
 📸 拍照分類
@@ -598,6 +599,7 @@ class LineMessageHandler:
         # 不顯示 confidence 給使用者
         flex_message = self._create_result_flex_message(classification_result, waste_info, texts, user_lang)
         self.line_bot_api.reply_message(reply_token, flex_message)
+
 
 
 
